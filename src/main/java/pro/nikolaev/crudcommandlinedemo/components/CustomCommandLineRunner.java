866c4e93081d4764c2ad208c58b3735d14d77ce2
realimp <impinbox@hotmail.com>
2020-01-29 08:46:31 +0300

    Moved CommandLineRunner to separate class

---
+++ src/main/java/pro/nikolaev/crudcommandlinedemo/components/CustomCommandLineRunner.java
@@ -0,0 +1,87 @@
+package pro.nikolaev.crudcommandlinedemo.components;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import pro.nikolaev.crudcommandlinedemo.services.DepartmentService;
+import pro.nikolaev.crudcommandlinedemo.services.EmployeeService;
+
+import java.util.Scanner;
+
+@Component
+public class CustomCommandLineRunner implements CommandLineRunner {
+
+    private static final String[] COMMANDS = new String[] {"CREATE", "DELETE", "FIND", "FIRE", "HIRE", "HELP", "LIST", "RENAME"};
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Добро пожаловать!\n" +
+                "Ниже представлен список доступных команд");
+        printHelp();
+
+        while (true) {
+            String userInput = scanner.nextLine().trim();
+            if (checkCommandInput(userInput)) {
+                String command = userInput.contains(" ") ? userInput.substring(0, userInput.indexOf(" ")) : userInput;
+                String commandParameter = userInput.replace(command, "").trim();
+                switch (command.toUpperCase()) {
+                    case "CREATE":
+                        System.out.println(departmentService.createDepartment(commandParameter, scanner));
+                        break;
+                    case "DELETE":
+                        System.out.println(departmentService.deleteDepartment(commandParameter, scanner));
+                        break;
+                    case "FIND":
+                        employeeService.findEmployee(commandParameter, scanner);
+                        break;
+                    case "FIRE":
+                        employeeService.fireEmployee(commandParameter);
+                        break;
+                    case "HIRE":
+                        employeeService.hireEmployee(commandParameter, scanner);
+                        break;
+                    case "HELP":
+                        printHelp();
+                        break;
+                    case "LIST":
+                        employeeService.listEmployeesByDepartment(commandParameter);
+                        break;
+                    case "RENAME":
+                        System.out.println(departmentService.renameDepartment(commandParameter));
+                        break;
+                }
+            }
+        }
+    }
+
+    private static boolean checkCommandInput(String command) {
+        command = command.contains(" ") ? command.substring(0, command.indexOf(" ")) : command;
+        for (int i = 0; i < COMMANDS.length; i++) {
+            if (command.equalsIgnoreCase(COMMANDS[i])) {
+                return true;
+            }
+        }
+        System.out.println("Команда " + command.toUpperCase() + " отсутствует! Введите HELP для просмтра списка команд.");
+        return false;
+    }
+
+    private static void printHelp(){
+        System.out.println(
+                "\tCREATE название отдела - создает новый отдел компании\n" +
+                        "\tDELETE название отдела - удаляет отдел компании\n" +
+                        "\tFIND имя сотрудника - поиск сотрудников по имени\n" +
+                        "\tFIRE id сотрудника - удалить сотрудника\n" +
+                        "\tHIRE название отдела - нанять сотрудника в отдел\n" +
+                        "\tHELP - отображает сообщение с описанием команд\n" +
+                        "\tLIST название отдела - показать список сотрудников отдела\n" +
+                        "\tRENAME старое название отдела: новое название отдела - переименовать отдел");
+    }
+}
